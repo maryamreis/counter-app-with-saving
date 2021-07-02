eea1cc0c014cb4b76e56d21979bfd46600346562
Maryam <maryamreis@hotmail.co.uk>
2021-07-02 14:19:43 +0100

    map

--- src/components/EmojiPicker.tsx
+++ src/components/EmojiPicker.tsx
@@ -3,51 +3,45 @@ import { useState} from "react";
 
 export default function EmojiPicker(): JSX.Element {
     const [emojiFromCurrentRender, queueRenderWithNewEmoji] = useState("⏰")
+    const [storedEmojisFromCurrentRender, queueRenderWithNewStoredEmojis] = useState<string[]>([]);
 
     const handleEmojiChangetoEgg = () => {
         queueRenderWithNewEmoji("🍳");
-        console.log(
-            "previous emoji:",
-            emojiFromCurrentRender,
-            "current emoji: 🍳",
-        );
     };
 
     const handleEmojiChangetoPlate = () => {
         queueRenderWithNewEmoji("🍽️");
-        console.log(
-            "previous emoji:",
-            emojiFromCurrentRender,
-            "current emoji: 🍽️",
-        );
     };
 
     const handleEmojiChangetoFace = () => {
         queueRenderWithNewEmoji("😋");
-        console.log(
-            "previous emoji:",
-            emojiFromCurrentRender,
-            "current emoji: 😋",
-        );
     };
 
     const handleEmojiChangetoClock = () => {
         queueRenderWithNewEmoji("⏰");
-        console.log(
-            "previous emoji:",
-            emojiFromCurrentRender,
-            "current emoji: ⏰",
-        );
     };
+
+    const handleStoreCurrentEmoji = () => {
+        queueRenderWithNewStoredEmojis(
+            [
+                ...storedEmojisFromCurrentRender,
+                emojiFromCurrentRender
+            ]
+        );
+    }
+
+    console.log(storedEmojisFromCurrentRender)
 
     return (
         <>
             <h1>Emoji Picker</h1>
+            <p>Stored emojis: {storedEmojisFromCurrentRender.slice(-5).map((emoji, index) => <li key={index}>{emoji}</li>)}</p>
             <p>Your emoji: {emojiFromCurrentRender}</p>
             <button onClick={handleEmojiChangetoClock}>⏰</button>
             <button onClick={handleEmojiChangetoEgg}>🍳</button>
             <button onClick={handleEmojiChangetoPlate}>🍽️</button>
             <button onClick={handleEmojiChangetoFace}>😋</button>
+            <button onClick={handleStoreCurrentEmoji}>Store currrent emoji</button>
         </>
     );
 }
